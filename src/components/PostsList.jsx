873cf1c66e96aea8fca9f1046b0c401584004f3f
vukosavljevic <domagojv2005@gmail.com>
2023-01-25 18:39:49 +0100

    feat: no posts message

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -24,9 +24,20 @@ function PostsList({ isVisible, onStopPosting }) {
     return (
         <>
             {modalContent}
-            <ul className={classes.posts}>
-                {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
-            </ul>
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
+                </ul>
+            )}
+            {
+                posts.length === 0 && (
+                    <div style={({ textAlign: 'center', color: 'white' })}>
+                        <h2>There are no posts yet.</h2>
+                        <p>Start adding some!</p>
+                    </div>
+                )
+            }
+
         </>
     )
 }
